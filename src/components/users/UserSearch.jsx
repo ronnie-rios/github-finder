914450ad2cbec60223ae5254fdf25c2d2135c776
Ronnie Rios <ronniesmail14@gmail.com>
2022-04-21 08:26:16 -0500

    usersearch component created with form and submit

---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+
+function UserSearch() {
+  const [text, setText] = useState('')
+
+  const { users } = useContext(GithubContext)
+  
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+
+      if (text === '') {
+          alert('please enter text')
+      } else {
+          //search for users
+          setText('')
+      }
+  }
+    return (
+    <div className='grid grid-cols-1 xl:grid-cls-2 lg:grid-cols-2 md:grid-cols-2 gaap-8 mb-8'>
+        <div>
+            <form onSubmit={handleSubmit}>
+            <div className="form-control">
+                <div className="relative">
+                    <input 
+                        type="text" 
+                        className="w-full pr-40 bg-gray-200 input input-lg text-black" 
+                        placeholder='Search'
+                        value={text}
+                        onChange={handleChange}
+                    />
+                    <button type='submit'className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">go</button>
+                </div>
+            </div>
+            </form>
+        </div>
+        {users.length > 0 &&(
+            <div>
+                <button className="btn btn-ghost btn-lg">Clear</button>
+            </div>
+        )}
+        
+    </div>
+  )
+}
+
+export default UserSearch
